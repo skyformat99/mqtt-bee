1b4f8c30ae7b30ed438537c70676fdb89234c3e6
Silvio Giebl <silvio.giebl@dc-square.de>
2018-05-02 14:27:26 +0200

    Improved memory footprint of IntMap #85 Removed ChunkBatch, only caching 1 chunk, no ThreadLocal + SoftReference Also improved freeing of ThreadLocals of Netty threads

--- src/main/java/org/mqttbee/util/collections/IntMap.java
+++ src/main/java/org/mqttbee/util/collections/IntMap.java
@@ -21,7 +21,6 @@ import org.mqttbee.annotations.NotNull;
 import org.mqttbee.annotations.Nullable;
 
 import javax.annotation.concurrent.NotThreadSafe;
-import java.util.ArrayDeque;
 
 /**
  * @author Silvio Giebl
@@ -34,11 +33,10 @@ public class IntMap<E> {
     private final int chunkMask;
     private final int hashMask;
 
-    private final Chunk<E>[] chunks;
-    private final int chunkBatchCount;
-    private ChunkBatch<E> chunkBatch;
+    private final Chunk[] chunks;
 
-    @SuppressWarnings("unchecked")
+    private Chunk freeChunk;
+
     public IntMap(final int maxKey) {
         final int pow2Bit = 32 - Integer.numberOfLeadingZeros(maxKey - 1);
         capacity = 1 << pow2Bit;
@@ -47,55 +45,56 @@ public class IntMap<E> {
         chunkMask = capacity - 1 - hashMask;
 
         chunks = new Chunk[(chunkMask >> chunkShift) + 1];
-        chunkBatchCount = 1 << (pow2Bit >> 2);
-        chunkBatch = new ChunkBatch<>(hashMask + 1, chunkBatchCount);
     }
 
     @Nullable
+    @SuppressWarnings("unchecked")
     public E put(final int key, @NotNull final E value) {
         checkKey(key);
         final int index = index(key);
-        Chunk<E> chunk = chunks[index];
+        Chunk chunk = chunks[index];
         if (chunk == null) {
-            chunk = chunkBatch.getChunk();
-            if (chunk == null) {
-                chunkBatch = new ChunkBatch<>(hashMask + 1, chunkBatchCount);
-                chunk = chunkBatch.getChunk();
-                assert chunk != null;
+            if (freeChunk == null) {
+                chunk = new Chunk(hashMask + 1);
+            } else {
+                chunk = freeChunk;
+                freeChunk = null;
             }
             chunks[index] = chunk;
         }
-        return chunk.put(key, hashMask, value);
+        return (E) chunk.put(key, hashMask, value);
     }
 
     @Nullable
+    @SuppressWarnings("unchecked")
     public E get(final int key) {
         checkKey(key);
-        final Chunk<E> chunk = chunks[index(key)];
+        final Chunk chunk = chunks[index(key)];
         if (chunk == null) {
             return null;
         }
-        return chunk.get(key, hashMask);
+        return (E) chunk.get(key, hashMask);
     }
 
     @Nullable
+    @SuppressWarnings("unchecked")
     public E remove(final int key) {
         checkKey(key);
         final int index = index(key);
-        final Chunk<E> chunk = chunks[index];
+        final Chunk chunk = chunks[index];
         if (chunk == null) {
             return null;
         }
-        final E value = chunk.remove(key, hashMask);
-        if (chunk.size() == 0) {
+        final E value = (E) chunk.remove(key, hashMask);
+        if (chunk.count == 0) {
+            freeChunk = chunk;
             chunks[index] = null;
-            chunk.returnToBatch();
         }
         return value;
     }
 
     private void checkKey(final int key) {
-        if (key > capacity || key < 0) {
+        if (key > capacity - 1 || key < 0) {
             throw new IllegalArgumentException();
         }
     }
@@ -105,82 +104,44 @@ public class IntMap<E> {
     }
 
 
-    private static class Chunk<E> {
+    private static class Chunk {
 
-        private final ChunkBatch<E> chunkBatch;
-        private final int offset;
-        private int size;
+        private final Object[] values;
+        private int count;
 
-        private Chunk(@NotNull final ChunkBatch<E> chunkBatch, final int offset) {
-            this.chunkBatch = chunkBatch;
-            this.offset = offset;
+        private Chunk(final int size) {
+            this.values = new Object[size];
         }
 
         @Nullable
-        private E put(final int key, final int hashMask, @NotNull final E value) {
+        Object put(final int key, final int hashMask, @NotNull final Object value) {
             final int index = index(key, hashMask);
-            final E[] values = chunkBatch.values;
-            final E previousValue = values[index];
+            final Object previousValue = values[index];
             values[index] = value;
             if (previousValue == null) {
-                size++;
+                count++;
             }
             return previousValue;
         }
 
         @Nullable
-        private E get(final int key, final int hashMask) {
-            return chunkBatch.values[index(key, hashMask)];
+        Object get(final int key, final int hashMask) {
+            return values[index(key, hashMask)];
         }
 
         @Nullable
-        private E remove(final int key, final int hashMask) {
+        Object remove(final int key, final int hashMask) {
             final int index = index(key, hashMask);
-            final E[] values = chunkBatch.values;
-            final E previousValue = values[index];
+            final Object previousValue = values[index];
             values[index] = null;
             if (previousValue != null) {
-                size--;
+                count--;
             }
             return previousValue;
         }
 
         private int index(final int key, final int hashMask) {
-            return offset + (key & hashMask);
-        }
-
-        private int size() {
-            return size;
-        }
-
-        private void returnToBatch() {
-            chunkBatch.returnChunk(this);
-        }
-
-    }
-
-
-    private static class ChunkBatch<E> {
-
-        private final E[] values;
-        private final ArrayDeque<Chunk<E>> availableChunks;
-
-        @SuppressWarnings("unchecked")
-        private ChunkBatch(final int chunkSize, final int chunkCount) {
-            values = (E[]) new Object[chunkCount * chunkSize];
-            availableChunks = new ArrayDeque<>(chunkCount);
-            for (int i = 0, offset = 0; i < chunkCount; i++, offset += chunkSize) {
-                availableChunks.offer(new Chunk<>(this, offset));
-            }
-        }
-
-        @Nullable
-        private Chunk<E> getChunk() {
-            return availableChunks.poll();
-        }
-
-        private void returnChunk(@NotNull final Chunk<E> chunk) {
-            availableChunks.offer(chunk);
+            return key & hashMask;
         }
 
     }
